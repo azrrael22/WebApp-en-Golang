f508dee5a575ae01742eb67bfc2d5875e7712fe0
Adriaann <adriaannm.rodriguezr@uqvirtual.edu.co>
2025-03-20 17:11:35 -0500

    Modificación para recibir el número de puerto como argumento de la línea de comandos.

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-const PORT string = ":8000"
+//const PORT string = ":8000"
 
 func main() {
-	fmt.Println("Servidor corriendo en puerto", PORT)
+
+	var puerto string
+	flag.StringVar(&puerto, "p", "localhost:8000", "numero de puerto")
+
+	flag.Parse()
+
+	fmt.Println("Servidor corriendo en puerto", puerto)
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hola Mundo")
 	})
 
-	log.Fatal(http.ListenAndServe(PORT, mux))
+	log.Fatal(http.ListenAndServe(puerto, mux))
 }
